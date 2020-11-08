f279d22fb9edb73529ded6b5ecc9122dc6100ce1
ofir <ofirsimhi1612@gmail.com>
2020-11-08 18:04:06 +0200

    AnalysticMachine error handeling

--- client/src/machines/analyticsMachine.ts
+++ client/src/machines/analyticsMachine.ts
@@ -3,16 +3,20 @@ import { httpClient } from "../utils/asyncUtils";
 import { isEmpty, omit } from "lodash/fp";
 import { assign } from "xstate";
 import { DataContext } from './dataMachine';
-import { concat } from "lodash/fp";
 
 export const analyticsMachine = dataMachine("analytics").withConfig({
   services: {
     fetchData: async (ctx, event: any) => {
-      const payload = omit("type", event);
-      const resp = await httpClient.get(`http://localhost:3001/events/${payload.params}`, {
-        params: !isEmpty(payload) ? payload.query : undefined,
-      });
-      return resp.data;
+      try{
+        const payload = omit("type", event);
+        const resp = await httpClient.get(`http://localhost:3001/events/${payload.params}`, {
+          params: !isEmpty(payload) ? payload.query : undefined,
+        });
+        return resp.data;
+      } catch(error){
+        console.log(error);
+      }
+      
     },
   },
   actions: {
